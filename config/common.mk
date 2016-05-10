5cae31a5036b03d5f56b72eed1b24a93ec95d066
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-10 20:04:18 +0200

    cm: Build DataUsageProvider
    Change-Id: I89124cd684f40b48099ce32be3e20111c044c8a0

--- config/common.mk
+++ config/common.mk
@@ -155,7 +155,8 @@ PRODUCT_PACKAGES += \
     CMSettingsProvider \
     ExactCalculator \
     LiveLockScreenService \
-    WeatherProvider
+    WeatherProvider \
+    DataUsageProvider
 
 # Exchange support
 PRODUCT_PACKAGES += \
