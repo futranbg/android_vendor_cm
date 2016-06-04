e1e44afe739e9189ce97d9436d9d0865ff393066
bangprovn <bangprovn@gmail.com>
2016-06-04 14:53:05 +0700

    CHANGELOG: updated
    Change-Id: Ia262748a17f28972cd42c30040d609aed13c552a
    
    (cherry picked from commit 8b68fbc4d998cbf91b8c26af83cc15c95d74c6b9)

--- config/common.mk
+++ config/common.mk
@@ -69,7 +69,7 @@ endif
 
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
-    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-TEKOS.txt
+    vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-XOS.txt
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
