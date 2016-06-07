3fd165f6740ab9d0d6a61026c7ff76579e89ad9a
d34d <clark@cyngn.com>
2016-06-07 15:39:13 -0700

    Bring back the custom wallpaper picker [5/5]
    Change-Id: I817be62290ee90c6ddcc662a893c4fd18c2ab9e8
    TICKET: OSS-62
    
    (cherry picked from commit 7454cd9981254c9b5c201b40cffc36a23f6cad43)

--- config/common.mk
+++ config/common.mk
@@ -161,7 +161,8 @@ PRODUCT_PACKAGES += \
     ExactCalculator \
     LiveLockScreenService \
     WeatherProvider \
-    DataUsageProvider
+    DataUsageProvider \
+    WallpaperPicker
 
 # Exchange support
 PRODUCT_PACKAGES += \
